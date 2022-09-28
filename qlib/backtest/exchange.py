a7a31d90cc6b501504f89021b488ff27a14d42b8
Chaos Yu <chaos.you@gmail.com>
2022-09-28 16:56:33 +0800

    Update exchange.py

--- qlib/backtest/exchange.py
+++ qlib/backtest/exchange.py
@@ -1,6 +1,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-from __future__ import annotations
 
 from collections import defaultdict
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Type, Union, cast
