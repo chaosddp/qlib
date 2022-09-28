cb373b2c1b067883dffc1ba0dcffe3bc7eec285a
Chaos Yu <chaos.you@gmail.com>
2022-09-28 16:49:16 +0800

    Update backtest.py

--- qlib/backtest/backtest.py
+++ qlib/backtest/backtest.py
@@ -1,8 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from __future__ import annotations
-
 from typing import TYPE_CHECKING, Generator, Optional, Tuple, Union, cast
 
 import pandas as pd
